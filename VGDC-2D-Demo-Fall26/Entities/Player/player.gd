3c0d6aafdf5550eaeac10a8f8eb5e494fe605a52
TagReaper <coxlucas2022@gmail.com>
2026-09-11 18:52:28 -0500

    feat: Finished Player Movement
    Player movement is done along with animation checks for motion and attacks.
    
    Attacks are implemented, but do not have hitboxes

--- VGDC-2D-Demo-Fall26/Entities/Player/player.gd
+++ VGDC-2D-Demo-Fall26/Entities/Player/player.gd
@@ -4,7 +4,8 @@ extends CharacterBody2D
 @export var speed: float = 20
 @export var max_health: float = 100
 @export var acceleration: float = 16
-@export var friction: float = 1
+@export var friction: float = 2
+@export var damage: float = 5
 
 # Node Variables
 @export var Sprite: AnimatedSprite2D
@@ -22,7 +23,77 @@ enum State {
 }
 
 func _ready() -> void:
-	pass
+	# Start Assignments
+	health = max_health
+	Sprite.play("Idle")
+	state = State.IDLE
 
 func _physics_process(delta: float) -> void:
+	# Idle Assignment
+	if state != State.ATTACK:
+		state = State.IDLE
+	
+	# Direction Calculations and Normalization
+	var direction = Vector2.ZERO
+	direction.x = Input.get_axis("MoveLeft", "MoveRight")
+	direction.y = Input.get_axis("MoveUp", "MoveDown")
+	direction = direction.normalized()
+	
+	# L/R Movement
+	if direction.x != 0 and state != State.ATTACK:
+		#State Assignment
+		state = State.RUN
+		
+		#Setting X-Velocity
+		velocity.x = move_toward(velocity.x, direction.x * speed * MOVEMENT_MULTIPLIER, acceleration)
+		
+		#Checking Direction
+		if direction.x > 0: #Facing Right
+			if Sprite.flip_h:
+				Sprite.flip_h = false
+		else: #Facing Left
+			if !Sprite.flip_h:
+				Sprite.flip_h = true
+	else:
+		velocity.x = move_toward(velocity.x, 0, acceleration * friction)
+	
+	# U/D Movement
+	if direction.y != 0 and state != State.ATTACK:
+		#State Assignment
+		state = State.RUN
+		
+		#Setting Y-Velocity
+		velocity.y = move_toward(velocity.y, direction.y * speed * MOVEMENT_MULTIPLIER, acceleration)
+	else:
+		velocity.y = move_toward(velocity.y, 0, acceleration * friction)
+	
+	# Attack
+	if Input.is_action_just_pressed("Attack") and state != State.ATTACK:
+		#Attack State Assignment
+		state = State.ATTACK
+		
+		#Attack Functionality
+		_attack()
+	
+	#Animation checking and Movement
+	_animation_check()
+	move_and_slide()
+
+func _animation_check():
+	match state:
+		State.IDLE:
+			if Sprite.animation != "Idle":
+				Sprite.play("Idle")
+		State.RUN:
+			if Sprite.animation != "Run":
+				Sprite.play("Run")
+		State.ATTACK:
+			if Sprite.animation != "Attack":
+				Sprite.play("Attack")
+
+func _attack():
 	pass
+
+func _animation_finished() -> void:
+	if Sprite.animation == "Attack":
+		state = State.IDLE
